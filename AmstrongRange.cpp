e32ca37ae32daead091c4f8c1aed6f177f1d0712
D Darshan <142141713+darshan106@users.noreply.github.com>
2025-01-13 23:03:45 +0530

    AmstrongRange.cpp

--- AmstrongRange.cpp
+++ AmstrongRange.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
     int start, end;
     cin >> start >> end;
-
     for (int i = start; i <= end; i++) {
         int num = i, sum = 0, rem = 0, temp = i;
         while (num != 0) {
@@ -16,6 +15,5 @@ int main() {
             cout << temp << " ";
         }
     }
-
     return 0;
 }
